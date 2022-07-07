72ff00c439d283148312040925b5ee805c483366
Islam Jihad <89529030+islamjihad@users.noreply.github.com>
2022-07-07 09:01:14 +0300

    Update data.js

--- ENAR/data.js
+++ ENAR/data.js
@@ -1,14 +1,13 @@
 const title = 'Birzeit University Department of Architectural Engineering Advisory Plan';
 const note = '(ENAR4300) ملاحظة التدريب العملي في صيف السنة الرابعة حسب الخطة بعد إتمام 90 ساعة';
-const link_title = '-- تفاصيل حول مساقات التركيز --';
-const url = 'httpsdrive.google.comfiled1eO7m7H1wFbwjzxwu6lwPcEcbwXWN7m5Aviewusp=sharing';
-const link_more_info = 'تحتوي أيضاً على خطة هندسة الكهرباء';  I DON'T KNOW WHAT I SHOULD WRITE HERE
+const link_title = ' ';
+const url = ' ';
+const link_more_info = ' ';  
 
 
 const courses = [
-    ['NAME', 'CODE', ['requirement']s, 'notes']
-
-    'year 1 semester 1',
+    //['NAME', 'CODE', ['requirement']s, 'notes']
+    //'year 1 semester 1',
     [' مهارات بصرية 1', 'ENAR1111', []],
     ['رسم معماري ', 'ENAR 1211',['ENAR1111']],
     ['مهارات اللغة العربية 1', 'ARAB135', []],
@@ -29,12 +28,12 @@ const courses = [
     'year 2 semester 1',
     ['تصميم معماري 1', 'ENAR2311', ['ENAR1311']],
     ['تاريخ عمارة 2', 'ENAR2351', ['ENAR1241']],
-    ['ظلال ومنظور', 'ENAR2211', ['ENAR1211']], DONE
-        ['تشيد مباني 1', 'ENAR2361', ['ENAR1211']],DONE
-        ['اخلاقيات مهنية', 'ENAR2111', []],DONE
-        ['استاتيكا', 'ENCE232', []],DONE
-        ['مهارات بصرية 2', 'ENAR2131', ['ENAR1111']],DONE
-        ['مختبر مواد بناء', 'ENCE216', ['ENAR1311']],DONE
+    ['ظلال ومنظور', 'ENAR2211', ['ENAR1211']],
+        ['تشيد مباني 1', 'ENAR2361', ['ENAR1211']],
+        ['اخلاقيات مهنية', 'ENAR2111', []],
+        ['استاتيكا', 'ENCE232', []],
+        ['مهارات بصرية 2', 'ENAR2131', ['ENAR1111']],
+        ['مختبر مواد بناء', 'ENCE216', ['ENAR1311']],
 
 
     'year 2 semester 2',
@@ -54,10 +53,10 @@ const courses = [
     ['تصميم معماري 3', 'ENAR3411', ['ENAR2321']],
     ['نظريات عمارة 1', 'ENAR3331', ['ENAR2381']],
     ['التخطيط العمراني1', 'ENPL3331', [' ENAR238']],
-    ['تشيد مباني3', 'ENAR3251', ['ENAR2371']],  PROBLEM HERE ENAR2471 OR ENAR2371
+    ['تشيد مباني3', 'ENAR3251', ['ENAR2471']],
         ['انظمة انشائية', 'ENCE2351', ['ENCE223']],
     ['مساحة', 'ENCE327', ['ENAR1211']],
-    ['مختبر مساحة', 'ENCE317', ['ENCE327']],  او متزامن
+    ['مختبر مساحة', 'ENCE317', ['ENCE327'],' او متزامن'],
 
 
 
@@ -66,7 +65,7 @@ const courses = [
     ['تصميم معماري 4', 'ENAR3421', ['ENCE2351','ENAR3411']],
     ['نظريات عمارة 2', 'ENAR3241', ['ENAR3331']],
     ['العمارة في فلسطين', 'ENAR3301', ['ENAR2381']],
-    ['الرسم بالحاسوب', 'ENAR3201', []],  60 HOURS SHOULD BE DONE
+    ['الرسم بالحاسوب', 'ENAR3201', [],'60 HOURS SHOULD BE DONE'],
         ['التخطيط العمراني 2', 'ENPL3341', ['ENPL3331']],
     ['خرسانة مسلحة', 'ENCE3361', ['ENCE3251']],
 
@@ -76,9 +75,9 @@ const courses = [
 
     ['تصميم معماري 5', 'ENAR441', ['ENPL3341','ENAR3421']],
     ['عمارة المشهد', 'ENPL436', ['ENPL3341','ENAR3421']],
-    ['هندسة اضاءة وصوت', 'ENEE3307', ['ENAR3251']],  OR متزامن
+    ['هندسة اضاءة وصوت', 'ENEE3307', ['ENAR3251'],'أو متزامن'],
         ['الاخراح بالحاسوب', 'ENAR4211', ['ENAR3201']],
-    ['ممارسة مهنة و ادارة مشاريع', 'ENCE4341', ['ENAR3251','ENAR2111']],  HERE THEY WRITE JUST 2111 SO I SUPPOSED THAT THEY ARE TALK ABOUT ENAR2111
+    ['ممارسة مهنة و ادارة مشاريع', 'ENCE4341', ['ENAR3251','ENAR2111']],
 
 
 
@@ -119,10 +118,6 @@ const courses = [
 
     'year 5 semester 2',
 
-    ['مهارات متقدمة', 'ENAR5111', []], الفصل االخير للتخرج
-        ['مشروع تخرج', 'ENAR530', ['ENAR520']], ,ENAR520 , ENAR5411 or ENAR5421 or ENAR5441
-
-
-
-
+    ['مهارات متقدمة', 'ENAR5111', []],
+    ['مشروع تخرج', 'ENAR530', ['ENAR520','ENAR5411','ENAR5421','ENAR5441']]
 ];
