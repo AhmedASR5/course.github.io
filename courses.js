d9065fe718127895200350fbf6fab8fd84b542a6
abdallahdaoud <abdallahdaoud37@gmail.com>
2022-07-11 14:19:58 +0300

    modified:   ACCT/index.html 	modified:   ACFI/index.html 	modified:   ENAR/index.html 	modified:   ENCS/index.html 	modified:   ENMC/index.html 	modified:   NUTD/index.html 	modified:   PHAR/index.html 	modified:   courses.css 	modified:   courses.js

--- courses.js
+++ courses.js
@@ -3,6 +3,8 @@ document.getElementById('link').children[0].innerHTML = link_title;
 document.getElementById('link').children[0].href = url;
 document.getElementById('link').title = link_more_info;
 
+// zoom parameter to determine what is the zoom size and to select the coures by click not by hover when zoom 
+var z = 1; 
 
 let str = '<h3 class="year">Year 1</h3><div class="semester1"><h4>Semester 1</h4>';
 let str2 = '', i = 2;
@@ -14,8 +16,10 @@ for(let course of courses) {
             + parseInt(i/2) + `</h3><div class="semester${i%2 + 1}"><h4>Semester 1</h4>`;
     }
     else {
-        str += `<${typeof(course[course.length - 1]) === 'number' ? 'section' : 'div'} id="${course[1]}" class="course" onmouseleave="Remove_highlights()" onmouseover="get_requirements(this)"><div>
-        ${course[0]}</div><div>${course[1]}</div>${course[3] ? `<div class="note">${course[3]}</div>`:''}</${typeof(course[course.length - 1]) === 'number' ? 'section' : 'div'}>`;
+        str += `<${typeof(course[course.length - 1]) === 'number' ? 'section' : 'div'} id="${course[1]}" 
+        class="course" onmouseleave="remove_highlights_normal_mode()" onmouseover="get_and_remove_normal_mode(this)" 
+        onclick="get_and_remove_zoom_mode(this)"><div>${course[0]}</div><div>${course[1]}</div>${course[3] ? `<div class="note">
+        ${course[3]}</div>`:''}</${typeof(course[course.length - 1]) === 'number' ? 'section' : 'div'}>`;
     }
 }
 document.getElementById('schedule').innerHTML = str2 + str + '</div>' 
@@ -24,11 +28,12 @@ document.getElementById('schedule').innerHTML = str2 + str + '</div>'
 document.getElementById('guide').style.gridColumn = `1/${i}`;
 if(i > 10) document.getElementsByTagName('html')[0].style.zoom = `${1/(i-.5)}`;
 else if(i <= 5) document.getElementById('guide').style.zoom = '.7';
-function Remove_highlights() { //Remove highlight from items
-    [...document.getElementsByTagName('div'), ...document.getElementsByTagName('section')].forEach(
-        course => {if(/course/.test(course.className)) course.className = 'course'});
-}
 
+function remove_highlights_normal_mode() { //Remove highlight from items
+    if(z < 1.01)
+        [...document.getElementsByTagName('div'), ...document.getElementsByTagName('section')].forEach(
+            course => {if(/course/.test(course.className)) course.className = 'course'});
+}
 function get_requirements(me) {
     [...document.getElementsByTagName('div'), ...document.getElementsByTagName('section')].forEach(
         course => {if(/course/.test(course.className)) course.className = 'course transparent'});
@@ -62,4 +67,34 @@ function get_requirements(me) {
     }
     //highlight requisite for items
     requisite_for.forEach(req => {document.getElementById(req).className = 'course requisite_for appear';});
+}
+function get_and_remove_normal_mode(me) {
+    if(z < 1.01) {
+        [...document.getElementsByTagName('div'), ...document.getElementsByTagName('section')].forEach(
+            course => {if(/course/.test(course.className)) course.className = 'course'});
+        get_requirements(me);
+    }
+}
+function get_and_remove_zoom_mode(me) {
+    if(z >= 1.01) {
+        [...document.getElementsByTagName('div'), ...document.getElementsByTagName('section')].forEach(
+            course => {if(/course/.test(course.className)) course.className = 'course'});
+        get_requirements(me);
+    }
+}
+function zoomin() {
+    if(z < 2) {
+        z = (z+.1)*1.1;
+        document.getElementById('schedule').style.zoom = z;
+        if(z > 2.2) document.getElementById('magnifier').style.textDecoration = 'line-through';
+    } 
+    if(z < 1.3) document.getElementById('smagnifier').style.textDecoration = 'none';
+}
+function zoomout() {
+    if(z > 1.2) {
+        z = z/1.1 - .1;
+        document.getElementById('schedule').style.zoom = z;
+        if(z < 1.02) document.getElementById('smagnifier').style.textDecoration = 'line-through';
+    } 
+    if(z > 1.9) document.getElementById('magnifier').style.textDecoration = 'none';
 }
